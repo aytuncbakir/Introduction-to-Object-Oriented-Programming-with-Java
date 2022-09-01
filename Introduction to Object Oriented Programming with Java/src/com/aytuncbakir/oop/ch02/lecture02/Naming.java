42d7566a2c451433972afec4906b2bf7753fe852
aytuncbakir <aytuncbakir.ab@gmail.com>
2022-09-01 20:12:45 +0300

    Ch02 - lecture02 - lecture03

---
+++ Introduction to Object Oriented Programming with Java/src/com/aytuncbakir/oop/ch02/lecture02/Naming.java
@@ -0,0 +1,27 @@
+package com.aytuncbakir.oop.ch02.lecture02;
+
+public class Naming {
+
+	// camelCase
+	// UpperCamelCase
+	// lowerCamelCase
+	
+	int x;
+	int myNumber;
+	boolean isBigger; 
+	
+	int sayı ;
+	double $fiyat;
+	
+	float sum_$;
+	int $$$_333;
+	int ala_ele_olo;
+	String _a_bcd_e_FGTH;
+	
+//	String 15aa;
+//	short  can/can; 
+//	byte lets-go;
+//	boolean true?;
+	
+//	int 1.wer;
+}
---
+++ Introduction to Object Oriented Programming with Java/src/com/aytuncbakir/oop/ch02/lecture03/Literals.java
@@ -0,0 +1,23 @@
+package com.aytuncbakir.oop.ch02.lecture03;
+
+public class Literals {
+	
+	
+	public void literalExamples() {
+//		Literals in Java 
+//		
+//		• Numbers
+//		  10, 5, 3.14,-1,
+//		• Characters
+//		  'a','x','z','d','-','.',';'
+//		• Strings
+//		  "String", "hello"
+//		• Logical literals
+//		  true, false
+		
+//		null
+		
+	}
+	
+
+}
